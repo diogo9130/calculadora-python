b860a9aa2454c4553aa3d9d7ec144a70d8691565
diogo silva <diogosilvastz@gmail.com>
2021-12-13 18:06:10 -0300

    calculadora python

---
+++ calculadora.py
@@ -0,0 +1,23 @@
+# Mini calculadora
+
+numero1 = float(input('Escolha o primeiro número: '))
+numero2 = float(input('Escolha o segundo número: '))
+operador = input('Escolha o operador desejado:')
+
+if operador == '+':
+    print('O operador escolhido foi +')
+    print(f'O resultado é: {numero1 + numero2}')
+    
+elif operador == '-':
+    print('O operador escolhido foi -')
+    print(f'O resultado é: {numero1 - numero2}')
+    
+elif operador == '/':
+    print('O operador escolhido foi /')
+    print(f'O resultado é: {numero1 / numero2}')
+
+elif operador == '*':
+    print('O operador escolhido foi *')
+    print(f'O resultado é: {numero1 * numero2}')
+
+
