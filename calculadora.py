9274c0118e8ca8e9898d8c0b27a7fd7926c52986
diogo silva <diogosilvastz@gmail.com>
2021-12-13 18:14:48 -0300

    calculadora python number-1

--- calculadora.py
+++ calculadora.py
@@ -12,12 +12,6 @@ elif operador == '-':
     print('O operador escolhido foi -')
     print(f'O resultado é: {numero1 - numero2}')
     
-elif operador == '/':
-    print('O operador escolhido foi /')
-    print(f'O resultado é: {numero1 / numero2}')
 
-elif operador == '*':
-    print('O operador escolhido foi *')
-    print(f'O resultado é: {numero1 * numero2}')
 
 
